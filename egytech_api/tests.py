c8ea9757a36c61f1eb89c10799ed91c370a73f42
Abdulrahman Mustafa Abdulrahman <abdulrahman.m.khaled@outlook.com>
2024-06-15 22:06:06 +0300

    Proper formatting of tests.py by ruff.
    Signed-off-by: Abdulrahman Mustafa Abdulrahman <abdulrahman.m.khaled@outlook.com>

--- egytech_api/tests.py
+++ egytech_api/tests.py
@@ -4,39 +4,59 @@ from pydantic import ValidationError
 from contextlib import nullcontext
 
 
-@pytest.mark.parametrize("normal, expected",
-                         [({"title": "backend", "min_yoe": 2, "business_line": "b2b"},
-                           nullcontext(ParticipantsQueryParams(
-                               title="backend",
-                               min_yoe=2,
-                               business_line="b2b"
-                           ))),
-                          ({"title": "frontend", "level": "intern", "max_yoe": 0}, pytest.raises(ValidationError)),
-                          ({"programming_language": "python"}, pytest.raises(ValidationError)),
-                          ({"title": ["backend"]}, pytest.raises(ValidationError)),
-                          ({"wrong_field": "backend"}, pytest.raises(ValueError)),
-                          ])
+@pytest.mark.parametrize(
+    "normal, expected",
+    [
+        (
+                {"title": "backend", "min_yoe": 2, "business_line": "b2b"},
+                nullcontext(
+                    ParticipantsQueryParams(
+                        title="backend", min_yoe=2, business_line="b2b"
+                    )
+                ),
+        ),
+        (
+                {"title": "frontend", "level": "intern", "max_yoe": 0},
+                pytest.raises(ValidationError),
+        ),
+        (
+                {"programming_language": "python"},
+                pytest.raises(ValidationError),
+        ),
+        ({"title": ["backend"]}, pytest.raises(ValidationError)),
+        ({"wrong_field": "backend"}, pytest.raises(ValueError)),
+    ],
+)
 class TestParticipantsQueryParams:
-
     def test_model_creation(self, normal, expected):
         with expected as e:
             participants = ParticipantsQueryParams(**normal)
             assert participants == e
 
 
-@pytest.mark.parametrize("normal, expected",
-                         [({"title": "backend", "min_yoe": 2, "business_line": "b2b"},
-                           nullcontext(StatsQueryParams(
-                               title="backend",
-                               min_yoe=2,
-                               business_line="b2b"
-                           ))),
-                          ({"title": "frontend", "level": "intern", "max_yoe": 0}, pytest.raises(ValueError)),
-                          ({"programming_language": "python"},
-                           nullcontext(StatsQueryParams(programming_language="python"))),
-                          ({"title": ["backend"]}, pytest.raises(ValidationError)),
-                          ({"wrong_field": "backend"}, pytest.raises(ValueError)),
-                          ])
+@pytest.mark.parametrize(
+    "normal, expected",
+    [
+        (
+                {"title": "backend", "min_yoe": 2, "business_line": "b2b"},
+                nullcontext(
+                    StatsQueryParams(
+                        title="backend", min_yoe=2, business_line="b2b"
+                    )
+                ),
+        ),
+        (
+                {"title": "frontend", "level": "intern", "max_yoe": 0},
+                pytest.raises(ValueError),
+        ),
+        (
+                {"programming_language": "python"},
+                nullcontext(StatsQueryParams(programming_language="python")),
+        ),
+        ({"title": ["backend"]}, pytest.raises(ValidationError)),
+        ({"wrong_field": "backend"}, pytest.raises(ValueError)),
+    ],
+)
 class TestStatsQueryParams:
 
     def test_model_creation(self, normal, expected):
